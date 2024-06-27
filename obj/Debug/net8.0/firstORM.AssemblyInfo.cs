ede2fdff76acb99c4304408b46c4f6de5b35eaef
Lucas Levorato <lucaslevorato@gmail.com>
2024-06-27 17:36:14 -0300

    services

--- obj/Debug/net8.0/firstORM.AssemblyInfo.cs
+++ obj/Debug/net8.0/firstORM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("firstORM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad7857959572b68c3585ed3bc41f1694abbcd58c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b9e964744d200bf38de5e078dfb89420e074328")]
 [assembly: System.Reflection.AssemblyProductAttribute("firstORM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("firstORM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
